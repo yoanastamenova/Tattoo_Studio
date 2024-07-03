75d2ef203f1881ae47d2b445a4f1fbd7e7d77e85
Yoana <yoanabarbie@icloud.com>
2024-07-03 18:06:36 +0200

    feat: added all of needed enpoints for the project

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'dotenv/config'
 import { AppDataSource } from './database/db';
 import { deleteUserById, getAllUsers, updateUserById } from './controllers/users.controller';
-import { register } from './controllers/auth.controller';
+import { register, userLogIn } from './controllers/auth.controller';
 import { createService, getAllServices } from './controllers/services.controller';
 
 const app = express();
@@ -11,26 +11,45 @@ app.use(express.json())
 const PORT = process.env.port || 4000;
 
 AppDataSource.initialize()
-  .then(() => {
-    console.log('Database connected');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
+    .then(() => {
+        console.log('Database connected');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
     })
-  })
-  .catch(error => {
-    console.log(error)
-  })
+    .catch(error => {
+        console.log(error)
+    })
 
-    // USERS CRUD
 
-    app.get('/users/', getAllUsers)      //To show all users in our DB in admin POV
-    app.post('/users/register', register)// to create newUser
-    app.put('/users/:id', updateUserById)  // to update User finded by ID
-    app.delete('/user/:id', deleteUserById)   //to eliminate User finded by ID
 
-    // SERVICES CRUD
+// AUTHENTICATION CRUD
 
-    app.get('/services', getAllServices)     // to see all the services
-    app.post('/services', createService) //to create a service
-    app.put('/services/:id')
-    app.delete('/services/:id')
+app.post('/register', register)       // to create new user
+app.post('/login', userLogIn)   //to log in to your account 
+
+
+// USERS CRUD
+
+app.get('/users/', getAllUsers)       //To show all users in our DB in admin POV
+app.get('/users/profile')             //To see user profile as user POV
+app.put('/users/profile')             // To modify(update) user profile as user POV
+
+
+  //additional USER CRUD
+app.put('/users/:id', updateUserById)  // to update User finded by ID
+app.delete('/user/:id/:role', deleteUserById)   //to eliminate User finded by ID
+
+
+//APPOINTMENTS CRUD
+app.post('/appointments')       //Create new appointment
+app.put('/appointments')        //Update an appointment
+app.get('/appointments/:id')       //Show an appointment by ID
+app.get('/appointments')          //Show all my appointments
+
+// SERVICES CRUD
+
+app.get('/services', getAllServices)     // to see all the services
+app.post('/services', createService) //to create a service
+app.put('/services/:id')       // to update a service by its ID
+app.delete('/services/:id')        // to delete a service by its ID
